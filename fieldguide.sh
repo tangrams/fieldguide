10c8eb9b620501589ecba31dc8657f512175f447
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-09-14 13:31:42 -0400

    installing pip

--- fieldguide.sh
+++ fieldguide.sh
@@ -1,7 +1,37 @@
 #!/usr/bin/env bash
+OS=$(uname)
+DIST="UNKNOWN"
+
+# what linux distribution is?
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DIST=$NAME
+fi
 
 case "$1" in
     install)
+
+        # INSTALL DEPENDECIES
+        if [ $OS == "Linux" ]; then
+            echo "Install dependeces for Linux - $DIST"
+
+            if [ "$DIST" == "Amazon Linux AMI" ]; then
+
+                # Amazon Linux
+                sudo yum update
+                sudo yum upgrade
+                sudo yum groupinstall "Development Tools"
+
+            elif [ "$DIST" == "Ubuntu" ]; then
+
+                sudo apt-get update
+                sudo apt-get upgrade
+                sudo apt-get install build-essential python-setuptools python-dev python-pip
+
+            fi
+        fi
+
+        
         if [ ! -d .tmp ]; then
             mkdir tmp
         fi
